404dadf841b3d96cd872017509fd79ee5ace67d8
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-08-22 13:26:43 +0200

    Added Constructor

---
+++ Benjamin.NortWindApp.Access/QueryExecutor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace Benjamin.NorthWindApp.DataAccess
+{
+    public class QueryExecutor
+    {
+        private String connectionString;
+
+        public QueryExecutor(string connectionString)
+        {
+            if( String.IsNullOrWhiteSpace(connectionString) )
+            {
+                throw new ArgumentException(nameof(connectionString) );
+            }
+
+            try
+            {
+                using( SqlConnection connection = new SqlConnection(connectionString) )
+                {
+                    connection.Open();
+                }
+            }
+            catch( ArgumentException ) { throw; }
+            catch( InvalidOperationException ) { throw; }
+            catch( SqlException ) { throw; }
+
+            this.connectionString = connectionString;
+        }
+        
+
+
+
+    }
+}
