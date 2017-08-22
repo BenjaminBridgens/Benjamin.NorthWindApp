5a879ebc125a34938e48726b20fc5f0e2c2c2277
Benjamin Joshua Bridgens <benj4183@edu.campusvejle.dk>
2017-08-22 13:15:34 +0200

    Fixed some Typo's

--- Benjamin.NortWindApp.Access/Class1.cs
+++ Benjamin.NortWindApp.Access/Class1.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Benjamin.NortWindApp.DataAccess
+namespace Benjamin.NorthWindApp.DataAccess
 {
     public class Class1
     {
